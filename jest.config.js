6b2a46ae89469e93e83a17eaccb6bbc7894f46e4
Enrique Rimando <me@elrmndo.dev>
2022-09-06 10:03:18 +0800

    test: fix jest config

--- jest.config.js
+++ jest.config.js
@@ -10,6 +10,9 @@ const createJestConfig = nextJest({
 const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   moduleDirectories: ['node_modules', '<rootDir>/src'],
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1',
+  },
   testEnvironment: 'jest-environment-jsdom',
 };
 
